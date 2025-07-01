c12a1e1a4768e0c8b703b382bfb72305997d53df
Rikartt <blennhemrikard@gmail.com>
2025-07-02 00:43:26 +0200

    made the js logic

--- JS/script.js
+++ JS/script.js
@@ -12,9 +12,9 @@ function CheckGrid (grid) {
                          [i-1, j+1], [i-1, j], [i-1, j-1]]
             for (let n = 0; n < neighbors.length; n++) {
                 let neighbor = neighbors[n]
-                neighbor[i] %= grid.length
-                neighbor[j] %= grid[0].length
-                if (grid[neighbor[i]][neighbor[j]]) {
+                neighbor[0] = (neighbor[0] + grid.length) % grid.length
+                neighbor[1] = (neighbor[1] + grid[0].length) % grid[0].length
+                if (grid[neighbor[0]][neighbor[1]]) {
                     liveneighborcount += 1
                 }
             }
@@ -31,7 +31,54 @@ function CheckGrid (grid) {
     return retgrid
 }
 
-let maingrid = createEmptyGrid(20, 20)
+function RandomizeGrid (grid) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            grid[i][j] = Math.random() < 0.5;
+        }
+    }
+    return grid
+}
+
+function RenderGrid(grid, elementid) {
+    var c = document.getElementById(elementid);
+    var ctx = c.getContext("2d");
+    var dwidth = c.width; var dheight = c.height; //Screen width and height
+    var tilewidth = dwidth / grid.length; var tileheight = dheight / grid[0].length; // Tile height and width
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j]) {
+                ctx.fillStyle = "white";
+            } else {
+                ctx.fillStyle = "black";
+            }
+            ctx.fillRect(i*tilewidth, j*tileheight, tilewidth, tileheight);
+        }
+    }
+}
+
+let desiredFPS = 30;
+let interval = 1000 / desiredFPS;
+let lastTime = 0;
+
+function mainloop(currentTime, grid) {
+    if (!lastTime) lastTime = currentTime;
+    let elapsed = currentTime - lastTime;
+
+    if (elapsed > interval) {
+        lastTime = currentTime;
+
+        RenderGrid(maingrid, "maincanvas");
+        maingrid = CheckGrid(maingrid);
+    }
+
+    requestAnimationFrame(mainloop);
+}
+
+
+
+
+let maingrid = RandomizeGrid(createEmptyGrid(300, 300));
 
 maingrid[1][2] = true
 maingrid[2][3] = true
@@ -41,3 +88,10 @@ maingrid[3][3] = true
 
 console.log(maingrid)
 console.log(CheckGrid(maingrid))
+//function mainloop() {
+    //RenderGrid(maingrid, "maincanvas");
+    //maingrid = CheckGrid(maingrid);
+    //requestAnimationFrame(mainloop); // Instead of infinite blocking while loop
+//}
+
+requestAnimationFrame(mainloop());
